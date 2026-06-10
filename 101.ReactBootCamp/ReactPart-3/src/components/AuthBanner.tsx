5eea390e32b6487a9f12ad9cb0739419014856fb
TryInDeep <164211616+TryInDeep@users.noreply.github.com>
2026-06-10 18:51:19 +0530

    Landig page completed

--- 101.ReactBootCamp/ReactPart-3/src/components/AuthBanner.tsx
+++ 101.ReactBootCamp/ReactPart-3/src/components/AuthBanner.tsx
@@ -1,12 +1,10 @@
 export function AuthBanner(){
     return (
         <div style={{minHeight: "100vh", backgroundColor: "black", display:"flex" ,alignItems: "center", justifyContent:"center"}}>
-            
             <div style={{display : "flex" ,alignItems:"center", flexDirection:"column"}}>
                 <img src="./logo.jpg" alt="" style={{width:200, height:200}}/>
-            <h1 style={{color: "white" , fontFamily: "Inter"}}>Build a board, get the job done!</h1>
+                <h1 style={{color: "white" , fontFamily: "Inter"}}>Build a board, get the job done!</h1>
             </div>
-            
         </div>
     )
 }
--- 101.ReactBootCamp/ReactPart-3/src/components/AuthCredentials.tsx
+++ 101.ReactBootCamp/ReactPart-3/src/components/AuthCredentials.tsx
@@ -1,7 +1,18 @@
 export function AuthCredentials(){
     return (
-        <div>
-            Auth Credentials Components
+        <div style={{minHeight:"100vh" , display: "flex", alignContent:"center", justifyContent: "center",alignItems:"center"}}>
+
+            <div style={{display:"flex" , justifyContent: "center", alignItems:"center", flexDirection: "column", }}>
+                <div style={{fontSize: 30}}>
+                    Login  in to Trello
+                </div>
+                <div>
+                    Connect to Neon with: 
+                </div>
+                <input type="text" placeholder="Email" />
+                <input type="text" placeholder="Password" />
+                <button> Sign In </button>
+            </div>
         </div>
     )
 }
