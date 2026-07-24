b37c68cb163537cf55464570978c92641a16d105
TryInDeep <164211616+TryInDeep@users.noreply.github.com>
2026-07-24 20:46:54 +0530

    added modules and learnings

---
+++ nextJs/reseveit-frontend-next/app/layout.tsx
@@ -0,0 +1,19 @@
+import "./globals.css";
+
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className={`h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col">
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ nextJs/reseveit-frontend-next/app/page.tsx
@@ -0,0 +1,11 @@
+import ChatBox from "@/components/ChatBox";
+
+export default function Home() {
+  return (
+    <div className="w-screen h-screen flex flex-col justify-center">
+      <div>
+        <ChatBox></ChatBox>
+      </div>
+    </div>
+  );
+}
